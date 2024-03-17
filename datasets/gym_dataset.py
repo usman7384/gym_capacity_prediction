c362b419b64f21171badc2fb4f0bb7bb7d0b9cc1
Asad <asadhayat_1@outlook.com>
2024-03-17 13:56:35 +0100

    updated dataset.py to make it more efficient

--- datasets/gym_dataset.py
+++ datasets/gym_dataset.py
@@ -1,4 +1,7 @@
 import numpy as np
+import pandas as pd
+import torch
+from torch.utils.data import Dataset
 
 class GymOccupancyDataset(Dataset):
     def __init__(self, csv_file, sequence_length=96, forecast_horizon=8, transform=None):
@@ -8,8 +11,8 @@ class GymOccupancyDataset(Dataset):
         self.forecast_horizon = forecast_horizon
 
         # Assuming 'timestamp' is not needed as a feature for model input
-        self.features = self.data_frame.drop(['occupancy_percentage', 'timestamp'], axis=1).values
-        self.labels = self.data_frame['occupancy_percentage'].values
+        self.features = self.data_frame.drop(['occupancy_percentage', 'timestamp'], axis=1)
+        self.labels = self.data_frame['occupancy_percentage']
 
     def __len__(self):
         # Adjust length to account for the sequence_length and forecast_horizon
@@ -18,12 +21,12 @@ class GymOccupancyDataset(Dataset):
     def __getitem__(self, idx):
         start = idx
         end = idx + self.sequence_length
-        features_sequence = self.features[start:end]
-        target_sequence = self.labels[end:end+self.forecast_horizon]
+        features_sequence = self.features.iloc[start:end].values
+        target_sequence = self.labels.iloc[end:end+self.forecast_horizon].values
 
         # Convert to tensor
-        features_sequence = torch.tensor(features_sequence, dtype=torch.float)
-        target_sequence = torch.tensor(target_sequence, dtype=torch.float)
+        features_sequence = torch.from_numpy(features_sequence).float()
+        target_sequence = torch.from_numpy(target_sequence).float()
 
         if self.transform:
             features_sequence = self.transform(features_sequence)
